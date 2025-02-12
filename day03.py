8b3307da444cb72ff2e9cb10ec0431fd4da42afc
deepblue <deepblue@inha.ac.kr>
2025-02-12 17:29:31 +0900

    v2.4 day03 assignment

--- day03.py
+++ day03.py
@@ -1,5 +1,5 @@
 # Assignment
-# v2.3) 다음 코드에서 딕셔너리를 제거하고 리스트만 사용하여 동일하게 동작하도록 코드를 수정하시오.
+# v2.4) 메뉴 삭제 추가에 대응되는 코드를 추가하시오.
 import random
 drinks = ["위스키", "와인", "소주", "고량주"]
 snacks = ["초콜릿", "치즈", "삽겹살", "양꼬치"]
@@ -7,9 +7,18 @@ snacks = ["초콜릿", "치즈", "삽겹살", "양꼬치"]
 drinks.append("사케")
 snacks.append("광어회")
 snacks[0] = "낙곱새"
+# drinks.append("데킬라")
+# snacks.append("소금")
 
+menu_list = '다음 술중에 고르세요.\n'
+for i in range(len(drinks)):
+    menu_list = menu_list + f'{i+1}) {drinks[i]}  '
+menu_list = menu_list + f'{len(drinks)+1}) 아무거나  {len(drinks)+2}) 종료 : '
+#print(menu_list)
+
+# f'다음 술중에 고르세요.\n1) {drinks[0]}   2) {drinks[1]}   3) {drinks[2]}   4) {drinks[3]}   5) {drinks[4]}   6) 아무거나   7) 종료 : '
 while True:
-    menu = input(f'다음 술중에 고르세요.\n1) {drinks[0]}   2) {drinks[1]}   3) {drinks[2]}   4) {drinks[3]}   5) {drinks[4]}   6) 아무거나   7) 종료 : ')
+    menu = input(menu_list)
     if menu == '1':
         print(f'{drinks[0]}에 어울리는 안주는 {snacks[0]} 입니다')
     elif menu == '2':
