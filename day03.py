873e0a28efeef7125ad2a44bc38315e24366ee19
deepblue <deepblue@inha.ac.kr>
2025-02-12 17:36:18 +0900

    v2.5 day03 assignment

--- day03.py
+++ day03.py
@@ -1,6 +1,7 @@
 # Assignment
-# v2.4) 메뉴 삭제 추가에 대응되는 코드를 추가하시오.
+# v2.5) while 안쪽의 하드코딩된 코드를 함수화하시오.
 import random
+
 drinks = ["위스키", "와인", "소주", "고량주"]
 snacks = ["초콜릿", "치즈", "삽겹살", "양꼬치"]
 
@@ -10,25 +11,27 @@ snacks[0] = "낙곱새"
 # drinks.append("데킬라")
 # snacks.append("소금")
 
+def print_menu(n):
+    print(f'{drinks[n]}에 어울리는 안주는 {snacks[n]} 입니다')
+
+
 menu_list = '다음 술중에 고르세요.\n'
 for i in range(len(drinks)):
     menu_list = menu_list + f'{i+1}) {drinks[i]}  '
 menu_list = menu_list + f'{len(drinks)+1}) 아무거나  {len(drinks)+2}) 종료 : '
-#print(menu_list)
 
-# f'다음 술중에 고르세요.\n1) {drinks[0]}   2) {drinks[1]}   3) {drinks[2]}   4) {drinks[3]}   5) {drinks[4]}   6) 아무거나   7) 종료 : '
 while True:
     menu = input(menu_list)
     if menu == '1':
-        print(f'{drinks[0]}에 어울리는 안주는 {snacks[0]} 입니다')
+        print_menu(int(menu)-1)
     elif menu == '2':
-        print(f'{drinks[1]}에 어울리는 안주는 {snacks[1]} 입니다')
+        print_menu(int(menu)-1)
     elif menu == '3':
-        print(f'{drinks[2]}에 어울리는 안주는 {snacks[2]} 입니다')
+        print_menu(int(menu)-1)
     elif menu == '4':
-        print(f'{drinks[3]}에 어울리는 안주는 {snacks[3]} 입니다')
+        print_menu(int(menu)-1)
     elif menu == '5':
-        print(f'{drinks[4]}에 어울리는 안주는 {snacks[4]} 입니다')
+        print_menu(int(menu)-1)
     elif menu == '6':
         random_index = random.randint(0, len(drinks)-1)
         print(f'{drinks[random_index]}에 어울리는 안주는 {snacks[random_index]} 입니다')
