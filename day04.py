b17f50a2c43d642802a51f1d6e7a6428f87e5f2f
deepblue <deepblue@inha.ac.kr>
2025-02-13 10:47:19 +0900

    v2.9 day04 kiosk

--- day04.py
+++ day04.py
@@ -6,17 +6,28 @@ import random
 drinks = ["위스키", "와인", "소주", "고량주"]
 snacks = ["초콜릿", "치즈", "삽겹살", "양꼬치"]
 prices = [50000, 30000, 5000, 7500]
+amounts = list()
+for i in range(len(drinks)):
+    amounts.append(0)
 
+print(amounts)
 drinks.append("사케")
 snacks.append("광어회")
 prices.append(25000)
+amounts.append(0)
 snacks[0] = "낙곱새"
 # drinks.append("데킬라")
 # snacks.append("소금")
 # prices.append(35000)
+# amounts.append(0)
+print(amounts)
+total_price = 0
 
-def print_menu(n):
+def print_menu_total_price(n):
+    global total_price
     print(f'{drinks[n]}에 어울리는 안주는 {snacks[n]} 입니다')
+    print(f'가격 : {prices[n]}')
+    total_price = total_price + prices[n]
 
 
 menu_list = '다음 술중에 고르세요.\n'
@@ -27,7 +38,7 @@ menu_list = menu_list + f'{len(drinks)+1}) 아무거나  {len(drinks)+2}) 종료
 while True:
     menu = int(input(menu_list))
     if 1 <= menu <= len(drinks):
-        print_menu(menu - 1)
+        print_menu_total_price(menu - 1)
     elif menu == len(drinks)+1:
         random_index = random.randint(0, len(drinks)-1)
         print(f'{drinks[random_index]}에 어울리는 안주는 {snacks[random_index]} 입니다')
