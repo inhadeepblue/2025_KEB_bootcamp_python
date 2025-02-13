da4984db79cbdf01ebd69a249fe75d63bacd287e
deepblue <deepblue@inha.ac.kr>
2025-02-13 14:27:40 +0900

    v3.4 day04 ocp decorator

--- day04.py
+++ day04.py
@@ -3,14 +3,25 @@
 import time
 
 
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'실행시간 : {e - s}초')
+        return r
+    return wrapper
+
+@time_decorator
 def factorial_repetition(n) -> int:
     result = 1
     for i in range(2, n+1):
         result = result * i
     return result
 
+
 number = int(input())
-s = time.time()
+# s = time.time()
 print(f"{number}! = {factorial_repetition(number)}")
-e = time.time()
-print(e-s)
+# e = time.time()
+# print(e-s)
