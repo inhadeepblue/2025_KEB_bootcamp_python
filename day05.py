4d07d3f0b39a1ede1d02af54144b8214d132ac17
deepblue <deepblue@inha.ac.kr>
2025-02-14 17:14:38 +0900

    v4.3 day05 module

--- day05.py
+++ day05.py
@@ -1,2 +1,4 @@
 import tgmath
 
+print(tgmath.tg_abs(-99))
+print(tgmath.tg_fibonacci_loop(6))
--- tgmath.py
+++ tgmath.py
@@ -1,4 +1,4 @@
-def my_abs(n) -> int:
+def tg_abs(n) -> int:
     """
     Return absolute value of parameter n
     :param n:
@@ -9,7 +9,30 @@ def my_abs(n) -> int:
     return n
 
 
+def tg_fibonacci_recursion(n) -> int:
+    """
+    Calculate fibonacci number (ver. recursive)
+    :param n:
+    :return: result value
+    """
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursion(n-2) + fibonacci_recursion(n-1)
 
+
+def tg_fibonacci_loop(n) -> int:
+    """
+    Calculate fibonacci number (ver. iterative)
+    :param n:
+    :return: result value
+    """
+    n_list=[0 ,1]
+    for i in range(n+1):
+        n_list.append(n_list[i] + n_list[i + 1])
+
+    return n_list[n]
 
 #print(my_abs(-99), my_abs(19), my_abs(-9119))
-
