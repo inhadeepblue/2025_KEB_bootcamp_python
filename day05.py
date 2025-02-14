d3cbb890144babd8fdae58d6680fb78aa38e94b6
deepblue <deepblue@inha.ac.kr>
2025-02-14 17:09:04 +0900

    v4.2 day05 module

--- day05.py
+++ day05.py
@@ -1,21 +1,2 @@
-def countdown_loop(n):
-    for i in range(n, -1, -1):
-        if i == 0:
-            print("펑~")
-        else:
-            print(i)
+import tgmath
 
-
-def countdown_recursion(n):
-    if n < 0:
-        return
-    if n == 0:
-        print("펑!")
-    else:
-        print(n)
-    countdown_recursion(n-1)
-
-
-n = int(input())
-#countdown_loop(n)
-countdown_recursion(n)
---
+++ tgmath.py
@@ -0,0 +1,15 @@
+def my_abs(n) -> int:
+    """
+    Return absolute value of parameter n
+    :param n:
+    :return: absolute value
+    """
+    if n < 0:
+        return -n
+    return n
+
+
+
+
+#print(my_abs(-99), my_abs(19), my_abs(-9119))
+
